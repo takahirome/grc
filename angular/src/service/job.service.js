3ff5b5be7b1ac557464269d8cdc30bbf1446b6f0
takaaki <t-kurasawa@t-kurasawa.local>
2017-05-02 22:47:15 +0900

    iconv

--- angular/src/service/job.service.js
+++ angular/src/service/job.service.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var jconv = require('jconv');
+var iconv = require('iconv-lite');
 
 // CRUDのR
 exports.job = function(req, res) {
@@ -15,9 +16,9 @@ exports.job = function(req, res) {
             // rejectされた時は、自動的にこの関数が中断されるからtry-catchで囲む必要もない。
             for(var i=0;i<callStocksResultsJson.length;i++){
                 // awaitで同期処理
-                const callScrapeResult = await callScrape(callStocksResultsJson[i].domain,jconv.encode(callStocksResultsJson[i].keyword,'JIS'));
-                console.log("---jconv.encode(callStocksResultsJson[i].keyword,'JIS')");
-                console.log(jconv.encode(callStocksResultsJson[i].keyword,'JIS'));
+                const callScrapeResult = await callScrape(callStocksResultsJson[i].domain,iconv.encode(callStocksResultsJson[i].keyword,'JIS'));
+                console.log("---iconv.encode(callStocksResultsJson[i].keyword,'JIS')");
+                console.log(iconv.encode(callStocksResultsJson[i].keyword,'JIS'));
                 process.on('unhandledRejection', console.dir);
                 if(callScrapeResult === "") break;
                 csrjson = JSON.parse(callScrapeResult);
