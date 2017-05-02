4d4e41767051883b58c35d890f3dd9193c81abc4
takaaki <t-kurasawa@t-kurasawa.local>
2017-05-02 19:30:52 +0900

    encodeURIComponent

--- lambda/app.js
+++ lambda/app.js
@@ -118,7 +118,7 @@ app.get('/scrape', (req, res) => {
 
   // Googleでkeywordを検索する。
   //
-  var request = { q: req.query.keyword };
+  var request = { q: encodeURIComponent(req.query.keyword) };
   var promise = searchClearlyByGoogle( request );
 
   // 検索結果を「タイトル」「URL」「概要」で表示する。
